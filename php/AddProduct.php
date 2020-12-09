2053b7cc9abcb4f929e4b6f7121fe666c8940437
jonterola <jonterola@gmail.com>
2020-12-09 21:21:57 +0100

    Añadir Producto XML

--- php/AddProduct.php
+++ php/AddProduct.php
@@ -1,1 +1,22 @@
-<?php ?>
+<?php
+$categoria = $_REQUEST['categoria'];
+$genero = $_REQUEST['genero'];
+$nombre = $_REQUEST['nombreProducto'];
+$precio = $_REQUEST['precio'];
+$stock = $_REQUEST['stock'];
+
+
+$xml = simplexml_load_file("../xml/productos.xml");
+
+$lastID = $xml['lastid'] + 1;
+$xml['lastid'] = $lastID;
+$producto = $xml->addChild('producto');
+$producto->addAttribute('id', $lastID);
+$producto->addChild('categoria',$categoria);
+$producto->addChild('genero',$genero);
+$producto->addChild('nombre',$nombre);
+$producto->addChild('precio',$precio);
+$producto->addChild('stock',$stock);
+
+$xml->asXML('../xml/productos.xml');
+?>
