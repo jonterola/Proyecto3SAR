8364138e4ab90c34df63602046d8371c38518072
Adrian <adrianruiz20001@gmail.com>
2020-12-11 21:00:34 +0100

    layout y css

---
+++ php/layout.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <LINK REL="stylesheet" TYPE="text/css" HREF="../styles/estilos.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+
+        <header>
+            <h1>TIENDA DE ROPA DEL TEAM GARDFOLD</h1>
+        </header>
+
+
+        <nav>
+            <ul>
+                <li><a href="#">Iniciar sesion</a></li>
+                <li><a href="#">Registrarse</a></li>
+            </ul>
+        </nav>
+
+        <section>
+            <h2>
+                ULTIMOS EN ENTRAR AL CATALOGO
+            </h2>
+            <img src="https://footwearnews.com/wp-content/uploads/2019/05/lightning-crocs.jpg" style="width: 900px;">
+        </section>
+
+        <aside>
+            <h4>Todos los productos</h4>
+            <h4>Novedades</h4>
+            <h4>Hombre</h4>
+            <h4>Mujer</h4>
+            <h4>Ofertas</h4>
+
+
+
+
+        </aside>
+
+        <footer>
+            <a href="http://www.internetedadinero.com">Ejemplo de Web Responsive - www.internetedadinero.com</a>
+        </footer>
+
+
+    </div>
+</body>
+
+</html>
