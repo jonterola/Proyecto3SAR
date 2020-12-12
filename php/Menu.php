acf042cf1ef4afb8b816a967c4d1492a94cbc499
Jon Tellechea <jonterola@gmail.com>
2020-12-12 15:32:41 +0100

    Separar menu y cabecera en ficheros aparte

---
+++ php/Menu.php
@@ -0,0 +1,15 @@
+<nav>
+    <ul>
+        <li><a href="#">Iniciar sesión</a></li>
+        <li><a href="#">Registrarse</a></li>
+    </ul>
+</nav>
+
+<aside>
+    <h4><a href="showStock.php">Todos los productos</a></h4>
+    <h4>Novedades</h4>
+    <h4><a href="showManStock.php">Hombre</a></h4>
+    <h4><a href="showWomanStock.php">Mujer</a></h4>
+    <h4>Ofertas</h4>
+
+</aside>
--- php/showStock.php
+++ php/showStock.php
@@ -4,32 +4,13 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <LINK REL="stylesheet" TYPE="text/css" HREF="../styles/estilos.css">
-</head>
-
-<body>
-
-    <div class="container">
-
-
-        <header>
-            <h1><a href="layout.php">TIENDA DE ROPA DEL TEAM GARDFOLD</a></h1>
-        </header>
-
-
-        <nav>
-            <ul>
-                <li><a href="#">Iniciar sesion</a></li>
-                <li><a href="#">Registrarse</a></li>
-            </ul>
-        </nav>
-
+<?php
+include '../html/Header.html';
+include '../php/Menu.php';
+ ?>
         <section>
             <h2>
-                PRODUCTOS DE HOMBRE
+                TODOS LOS PRODUCTOS
             </h2>
         <?php
 
@@ -55,24 +36,6 @@
         ?>
     </section>
 
-<aside>
-    <h4><a href="showStock.php">Todos los productos<a></h4>
-    <h4>Novedades</h4>
-    <h4><a href="showManStock.php">Hombre</a></h4>
-    <h4><a href="showWomanStock.php">Mujer</a></h4>
-    <h4>Ofertas</h4>
-
-
-
-
-</aside>
-
-<footer>
-    
-</footer>
-
-
-</div>
-</body>
+    <?php include '../html/Footer.html';?>
 
 </html>
